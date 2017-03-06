9efbac9dc4792d624900e1bfa0b5da541ff24e60
vladek1934 <vlad.mun1999@gmail.com>
2017-03-06 15:27:10 +0600

    1 and 3

---
+++ 3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+namespace _3
+{
+    class Program
+    {
+        public static void print() 
+        {
+            int x = 1;
+            while (true)
+            {
+                Thread.Sleep(200);
+                
+                double y = Math.Sin(10*x * Math.PI / 18);
+                int b = Convert.ToInt32(y);
+                Console.SetCursorPosition( 2*x , 2*b+20);
+                Console.Write("*");
+                x = x + 1;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(new ThreadStart(print));
+            t.Start();
+            
+               
+        }
+    }
+}
---
+++ Midterm exam VladMun/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Midterm_exam_VladMun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+           
+          
+           
+               
+                for (int i = 0; i < args.Length; i++)
+                { int chislo = int.Parse(args[i]);
+                    if ((chislo%3==0)&&(chislo%5!=0)){Console.WriteLine(chislo);}
+                    }
+                    return ;
+
+        }
+    }
+}
